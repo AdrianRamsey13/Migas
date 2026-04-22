c8dc27274166e2ee7def707ecd5021a278060e74
AdrianRamsey <105219057@student.universitaspertamina.ac.id>
2026-04-22 22:16:51 +0700

    Work Orders Page

---
+++ app/Filament/Resources/WorkOrderResource.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\WorkOrderResource\Pages;
+use App\Filament\Resources\WorkOrderResource\RelationManagers;
+use App\Models\WorkOrder;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class WorkOrderResource extends Resource
+{
+    protected static ?string $model = WorkOrder::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('wo_number')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('asset_id')
+                    ->relationship('asset', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('type')
+                    ->required(),
+                Forms\Components\TextInput::make('priority')
+                    ->required(),
+                Forms\Components\TextInput::make('status')
+                    ->required(),
+                Forms\Components\TextInput::make('requested_by')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('assigned_to')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\DatePicker::make('scheduled_date'),
+                Forms\Components\DatePicker::make('completed_date'),
+                Forms\Components\Textarea::make('notes')
+                    ->columnSpanFull(),
+                Forms\Components\DateTimePicker::make('started_at'),
+                Forms\Components\DateTimePicker::make('completed_at'),
+                Forms\Components\DateTimePicker::make('closed_at'),
+                Forms\Components\Textarea::make('rejection_reason')
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('wo_number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('asset.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('type'),
+                Tables\Columns\TextColumn::make('priority'),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('requested_by')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('assigned_to')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('scheduled_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('completed_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('started_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('completed_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('closed_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListWorkOrders::route('/'),
+            'create' => Pages\CreateWorkOrder::route('/create'),
+            'edit' => Pages\EditWorkOrder::route('/{record}/edit'),
+        ];
+    }
+}
